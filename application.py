f52500b4e9a7d49caee12de201a8be67e604b05b
DanielZh <haveacar.zhovnir@gmail.com>
2024-02-07 14:45:38 +0200

    db initialize

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from models import db
 
 # set up flask server
 application = Flask(__name__)
@@ -15,6 +16,9 @@ application.config["JWT_SECRET_KEY"] = 'my jwt key from secret manager'
 # Database configuration
 application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://your_user:your_password@your_host/your_database'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# initialize db
+db.init_app(application)
 
 if __name__ == '__main__':
     application.run(debug=True, port=5000)
