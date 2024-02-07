3dafddcec0bd5d5e957e96b467c8615aeabc451e
DanielZh <haveacar.zhovnir@gmail.com>
2024-02-07 20:22:00 +0200

    add constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+HTML_CONFIRM = """\
+                <html>
+                  <body>
+                    <p>Hi,<br>
+                       Please click the following link to confirm your email:<br>
+                       {}
+                    </p><br>
+                    <p>
+                        Best regrets,<br>
+                        Web3m Team
+                    </p>
+                  </body>
+                </html>
+                """
