56e2790d4664e69136ebad838e7bbca447ffa4cf
DanielZh <haveacar.zhovnir@gmail.com>
2024-02-07 13:46:44 +0200

    set up flask backend/001

--- application.py
+++ application.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
+# set up flask server
 application = Flask(__name__)
+CORS(application)
+api = Api(application)
+jwt = JWTManager(application)
+# set up keys from aws secret manager
+application.config['SECRET_KEY'] = 'my secret key from secret manager'
+application.config["JWT_SECRET_KEY"] = 'my jwt key from secret manager'
 
 # Database configuration
 application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://your_user:your_password@your_host/your_database'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-application.config['SECRET_KEY'] = 'my key from secret manager'
-
 
 if __name__ == '__main__':
-    application.run(debug=False, port=5000)
+    application.run(debug=True, port=5000)
